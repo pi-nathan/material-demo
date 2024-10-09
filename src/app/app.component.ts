48dc3c4d9fb799fd24e2648391ca940f610b18f5
Nathan Smith <nathan.smith@payrollintegrations.com>
2024-10-09 16:39:23 -0400

    sample components using inter font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { AddressComponent } from './address/address.component';
 import { Component } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
 import { RouterOutlet } from '@angular/router';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, AddressComponent, TableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'material-demo';
